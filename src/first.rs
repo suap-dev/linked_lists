bb4e7423cb6f7db953cf93d38f97ea5bf1133de3
suap-dev <suap.dev@gmail.com>
2023-11-09 02:00:15 +0100

    basic test

--- src/first.rs
+++ src/first.rs
@@ -11,3 +11,25 @@ enum Link {
     Empty,
     More(Box<Node>),
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let mut x = List { head: Link::Empty };
+        assert!(matches!(x.head, Link::Empty));
+
+        x.head = Link::More(Box::new(Node {
+            elem: 3,
+            next: Link::Empty,
+        }));
+        let y = if let Link::More(b) = x.head {
+            Some(b.elem)
+        } else {
+            None
+        };
+        assert_eq!(y.unwrap(), 3);
+    }
+}
